d3f950c7c4471046eeb4e7d95dde0baf1bbf01ff
mattbenka <m.benka@bath.ac.uk>
2025-08-17 16:27:32 +0100

    Finished file writing function

--- functions/write_file.py
+++ functions/write_file.py
@@ -3,8 +3,19 @@ import os
 
 def write_file(working_directory, file_path, content):
     abs_working_dir = os.path.abspath(working_directory)
-    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    target_file = os.path.abspath(os.path.join(abs_working_dir, file_path))
     if not target_file.startswith(abs_working_dir):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(target_file):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
+    if not os.path.exists(target_file):
+        try:
+            os.makedirs(os.path.dirname(target_file), exist_ok=True)
+        except Exception as e:
+            return f"Error: creating directory: {e}"
+    if os.path.exists(target_file) and os.path.isdir(target_file):
+        return f'Error: "{file_path}" is a directory, not a file'
+    try:
+        with open(target_file, "w") as f:
+            f.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: {e}"    
--- tests.py
+++ tests.py
@@ -1,6 +1,17 @@
+from functions.write_file import write_file
 from functions.get_file_content import get_file_content
 from functions.get_files_info import get_files_info
 
+result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+print(result)
+result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+print(result)
+result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+print(result)
+
+
+
+'''
 result = get_file_content("calculator", "main.py")
 print(result)
 
@@ -13,10 +24,6 @@ print(result)
 result = get_file_content("calculator", "pkg/does_not_exist.py")
 print(result)
 
-
-
-
-'''
 result = get_file_content("calculator", "lorem.txt")
 print(result)
 
